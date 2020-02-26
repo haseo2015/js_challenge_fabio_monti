3ddd9cc1f1fed2922e3f164aec1a6139f5348704
fabio.monti@gmail.com <fabio.monti@gmail.com>
2020-02-26 19:03:31 +0100

    Woops App left alone...

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,47 @@
     <Header />
     <main class="product-page">
       <div class="container">
-      <List :items="products"/>
+        <List :items="products"/>
+        <Pagination
+          :pages="pages" />
       </div>
     </main>
   </div>
 </template>
 
 <script>
+/**
+ * @component App
+ * @method getProducts @param str (the string to be appended to the url) @returns Object
+ * @beforeMount listen to the CHANGE_PAGE event to get the new product
+ * @mounted get all the products to init the pagination components and sets the firsts products
+ */
 import api from '@/api'
 import List from '@/components/List'
-
+import eventBus from '@/eventBus'
 export default {
   name: 'App',
   components: { List },
   data: () => ({
+    pages: 0,
+    size: 6,
     products: []
   }),
+  methods: {
+    async getProducts (str) {
+      await api.getProducts(str).then(response => (this.products = response))
+    }
+  },
   beforeMount () {
-    api.getProducts().then(response => (this.products = response))
+    const that = this
+    eventBus.$on('CHANGE_PAGE', offset => {
+      that.getProducts('?limit=6&offset=' + offset)
+    })
+  },
+  async mounted () {
+    await this.getProducts()
+    this.pages = Math.round(this.products.length / this.size)
+    this.products = this.products.slice(0, this.size)
   }
 }
 </script>
